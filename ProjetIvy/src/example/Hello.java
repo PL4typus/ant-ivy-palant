cbd3ada9867da25cfe35272b1c29b322d3bcf412
PL <pierre-louis.palant@insa-cvl.fr>
2017-04-11 10:53:20 +0200

    Marche sur le papier mais le data.csv ne marche pas

--- ProjetIvy/src/example/Hello.java
+++ ProjetIvy/src/example/Hello.java
@@ -6,9 +6,7 @@ import java.io.IOException;
 import java.io.FileNotFoundException;
 import org.apache.commons.lang.WordUtils;
 import com.opencsv.*;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Collections;
+import java.util.*;
 
 public class Hello {
     public static void main(String[] args) {
@@ -16,21 +14,30 @@ public class Hello {
         System.out.println("standard message : " + message);
         System.out.println("capitalized by " + WordUtils.class.getName() 
                                 + " : " + WordUtils.capitalizeFully(message));
-        int monmax = 0;
+        int unMax = 0;
         try{
             CSVReader reader = new CSVReader(new FileReader("data.csv"));
             try{
                 List<String[]> myEntries = reader.readAll();
                 for (String[] strings : myEntries) {
                     System.out.println(Arrays.toString(strings));
+                    for(String nb : strings)
+                    {
+                        unMax = max(unMax, Integer.parseInt(nb));
+                    }
                 }
-                System.out.println(collections.max(Integer.parseInt(myEntries)));
+                System.out.println("Max : "+ unMax);
             }catch (IOException e){
                 System.out.println("standard message: IOE");
             }
         }catch(FileNotFoundException e){
             System.out.println("standard message :FNFE ");
         }
+    }
+    public static int max(int a, int b)
+    {
+            return a > b ? a : b;
+            //return a; //le stagiaire est par la
     }
 }
         
